5fb02b315a3476ccaedfbafb6faac585e9833c04
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-05 18:33:55 +0100

    .rspec file added

--- spec/SecretDiary_spec.rb
+++ spec/SecretDiary_spec.rb
@@ -1,5 +1,7 @@
 require './SecretDiary'
 
 describe SecretDiary do
-    
+  it "will raise an error when trying at access the diary if locked" do
+		expect { subject.add_entry }.to raise_error("Please unlock the diary")
+	end 
 end
