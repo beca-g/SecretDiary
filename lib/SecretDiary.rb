fdf9a5d8baadc018a22bae325f17b2f0140f80d5
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-05 18:47:25 +0100

    REFACTOR: Added code for #add_entry. Need more tests based on code.
    .
    
    Finished in 0.01155 seconds (files took 0.16396 seconds to load)
    1 example, 0 failures

--- lib/SecretDiary.rb
+++ lib/SecretDiary.rb
@@ -1,4 +1,11 @@
 class SecretDiary
+  def initialize
+    @diary_entires = []
+  end
+
   def add_entry(diary_entry)
+    fail "Please unlock the diary" unless unlock
+    
+    diary_entires << diary_entry
   end
 end
