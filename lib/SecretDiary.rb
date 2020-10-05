20d610207027c2c9668514777afd749c43a59c73
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-05 21:27:36 +0100

    GREEN/REFACTORED: Passed test, will need further tests added once lock has been made.
    .
    
    Finished in 0.00517 seconds (files took 0.08727 seconds to load)
    1 example, 0 failures

--- lib/SecretDiary.rb
+++ lib/SecretDiary.rb
@@ -1,11 +1,18 @@
 class SecretDiary
   def initialize
     @diary_entires = []
+    @locked = true
+  end
+
+  def locked?
+  end
+
+  def unlocked?
   end
 
   def add_entry(diary_entry)
-    fail "Please unlock the diary" unless unlock
+    return "Please unlock the diary" if @unlocked == true
     
-    diary_entires << diary_entry
+    @diary_entires << diary_entry
   end
 end
